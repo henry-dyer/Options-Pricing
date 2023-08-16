ed2dc1ccc7a0e9b46c460265561a7de48aed10ad
henrydyer <henry.dyer@colorado.edu>
2023-08-16 12:29:03 -0600

    Modifed Binomial Pricing and implimented most of the european trinomial pricing model

--- Source/european.py
+++ Source/european.py
@@ -19,40 +19,68 @@ class European:
         else:
             return self.S * np.exp(-self.r * self.T) * norm.cdf(-d2) - self.S * norm.cdf(-d1)
 
-    def binomial(self, num_steps=252):
-        delta_t = self.T / num_steps
-        u = np.exp(self.sigma * np.sqrt(delta_t))
+    def binomial(self, n=252):
+        dt = self.T / n
+        u = np.exp(self.sigma * np.sqrt(dt))
         d = 1 / u
-        p = (np.exp(self.r * delta_t) - d) / (u - d)
+        p = (np.exp(self.r * dt) - d) / (u - d)
         q = 1 - p
 
-        stock_price = [[0 for j in range(i + 1)] for i in range(num_steps + 1)]
+        stock_price = [[0 for j in range(i + 1)] for i in range(n + 1)]
         stock_price[0][0] = self.S
 
-        for i in range(1, num_steps + 1):
+        for i in range(1, n + 1):
             for j in range(i + 1):
                 if j == 0:
                     stock_price[i][j] = stock_price[i - 1][j] * u
                 else:
                     stock_price[i][j] = stock_price[i - 1][j - 1] * d
 
-        option_value = [[0 for j in range(i + 1)] for i in range(num_steps + 1)]
+        option_value = [[0 for j in range(i + 1)] for i in range(n + 1)]
 
-        for j in range(num_steps + 1):
+        for j in range(n + 1):
             if self.call:
-                option_value[num_steps][j] = max(stock_price[num_steps][j] - self.K, 0)
+                option_value[n][j] = max(stock_price[n][j] - self.K, 0)
             else:
-                option_value[num_steps][j] = max(self.K - stock_price[num_steps][j], 0)
+                option_value[n][j] = max(self.K - stock_price[n][j], 0)
 
-        for i in range(num_steps - 1, -1, -1):
+        for i in range(n - 1, -1, -1):
             for j in range(i + 1):
-                option_value[i][j] = np.exp(-self.r * delta_t) * (p * option_value[i + 1][j] + q * option_value[i + 1][j + 1])
+                option_value[i][j] = np.exp(-self.r * dt) * (p * option_value[i + 1][j] + q * option_value[i + 1][j + 1])
 
         return option_value[0][0]
 
-    def trinomial(self, num_steps):
-        # Implement the binomial tree pricing method here
-        pass
+    def trinomial(self, n=252):
+        dt = self.T / n
+        up_factor = np.exp(self.sigma * np.sqrt(2 * dt))
+        down_factor = 1 / up_factor
+
+        p_u = ((np.exp(self.r * dt / 2) - np.exp(-self.sigma * np.sqrt(dt / 2))) / (np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(-self.sigma * np.sqrt(dt / 2))))**2
+        p_d = ((np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(self.r * dt / 2)) / (np.exp(self.sigma * np.sqrt(dt / 2)) - np.exp(-self.sigma * np.sqrt(dt / 2))))**2
+        p_m = 1 - p_u - p_d
+
+        stock_price = [[0 for j in range(2 * i + 1)] for i in range(n + 1)]
+        stock_price[0][0] = self.S
+
+        for i in range(1, n + 1):
+            for j in range(2 * i + 1):
+                if j == 0:
+                    stock_price[i][j] = stock_price[i - 1][j] * up_factor
+                elif j == (2 * i):
+                    stock_price[i][j] = stock_price[i - 1][j - 2] * down_factor
+                else:
+                    stock_price[i][j] = stock_price[i - 1][j - 1]
+
+        option_value = [[0 for j in range(2 * i + 1)] for i in range(n + 1)]
+
+        for j in range(2 * n + 1):
+            option_value[n][j] = max(stock_price[n][j] - self.K, 0)
+
+        for i in range(n - 1, -1, -1):
+            for j in range(2 * i + 1):
+                option_value[i][j] = np.exp(-self.r * dt) * (p_u * option_value[i+1][j] + p_m * option_value[i+1][j+1] + p_d * option_value[i+1][j+2])
+
+        return option_value[0][0]
 
     def monte_carlo(self, sims=100000):
         option_value = np.zeros(sims)
@@ -95,6 +123,7 @@ option = European(spot, strike, time, rate, vol, False)
 bs = option.black_scholes()
 mc = option.monte_carlo()
 bi = option.binomial()
+tri = option.trinomial()
 
-print("Black-Scholes Call Price:", bs, mc, bi)
+print("Black-Scholes Call Price:", bs, mc, bi, tri)
 
